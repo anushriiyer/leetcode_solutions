9ed8e1ac0c857c22f2e185af260f49a5ceb48f6f
Anushri Iyer <anushriiiyer@gmail.com>
2024-12-24 17:43:35 +0530

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -8,6 +8,7 @@ Input: nums = [2,2,1]
 Output: 1
 '''
 
+#Apprach 1: Uses O(N) space complexity
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         count = {}
@@ -15,3 +16,15 @@ class Solution:
             count[num]= count.get(num,0)+1
         return [k for k,v in count.items() if v==1][0] #return first element of list that is retrieved where v is 1
         
+#Approach 2: Use bitwise operator
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result^=num
+        return result
+
+'''
+same number XOR will give 0
+0 XOR with number gives number
+'''
